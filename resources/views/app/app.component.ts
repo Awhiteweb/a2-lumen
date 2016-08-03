0a9264afd7d5d50a040c92b151b08888bfa3f167
Alex White <alex_p_white@yahoo.co.uk>
2016-08-03 07:30:25 +0100

    initial commit of angular 2 lumen website

---
+++ resources/views/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+//import { Router } from '@angular/router';
+//import { Link } from './classes/link.entity';
+
+@Component({
+	selector: 'my-app',
+	template: '<h1>test</h1>'
+})
+
+export class AppComponent {
+//	links = [
+//		{ "name": "home", "address": "home" }
+//	];
+}
